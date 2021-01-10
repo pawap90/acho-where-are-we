7c12776e8938b8d1f655085cb7a768f4bf1b9eee
paulasantamaria <m_paula_santamaria@live.com>
2021-01-10 20:41:07 -0300

    Add jsdocs and improve naming

--- popup.js
+++ popup.js
@@ -1,19 +1,26 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     const dialogBox = document.getElementById('dialog-box');
     const query = { active: true, currentWindow: true };
 
     chrome.tabs.query(query, (tabs) => {
-        dialogBox.innerHTML = barkTitle(tabs[0].title);
+        dialogBox.innerHTML = getBarkedTitle(tabs[0].title);
     });
 });
 
-function barkTitle(title) {
-    const barkTitle = `${getRandomBark()} Ahem.. I mean, we are at: ${title}`
+/**
+ * Concatenates the tab title with Acho's barks.
+ * @param {String} tabTitle Current tab title
+ */
+const getBarkedTitle = (tabTitle) => {
+    const barkTitle = `${getRandomBark()} Ahem.. I mean, we are at: ${tabTitle}`
     return barkTitle;
 }
 
+/**
+ * List of possible barks.
+ */
 const barks = [
     'Barf barf!',
     'Birf birf!',
@@ -23,7 +30,10 @@ const barks = [
     'Biiiirf!'
 ]
 
-function getRandomBark() {
+/**
+ * Returns a random bark from the list of possible barks.
+ */
+const getRandomBark = () => {
     const bark = barks[Math.floor(Math.random() * barks.length)];
     return bark;
 }
