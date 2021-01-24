6d2ce1ff1c31c5abbf8fc894ede6ca6c228ac29e
paulasantamaria <m_paula_santamaria@live.com>
2021-01-24 19:23:29 -0300

    Show notification

--- 03-notifications/content.js
+++ 03-notifications/content.js
@@ -1,4 +1,35 @@
+
+// Notification body.
+const notification = document.createElement("div");
+notification.className = 'acho-notification';
+
+// Notification icon.
+const icon = document.createElement('img');
+icon.src = chrome.runtime.getURL("images/icon32.png");
+notification.appendChild(icon);
+
+// Notification text.
+const notificationText = document.createElement('p');
+notificationText.className = 'acho-notification-text';
+notification.appendChild(notificationText);
+
+// Add to current page.
+document.body.appendChild(notification);
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    alert("Tab title: " + request.tabTitle);
+
+    const notification = document.getElementsByClassName('acho-notification')[0];
+    const notificationText = document.getElementsByClassName('acho-notification-text')[0];
+    notificationText.innerText = `${request.tabTitle}`;
+
+    notification.style.display = 'flex';
+
+    setTimeout(function () {
+
+        notification.style.display = 'none';
+    }, 3000);
+
+
+
     return true;
 });
